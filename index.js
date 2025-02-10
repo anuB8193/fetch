7db30a99bdc555af406b5c51b6fa38a2f8f1a16f
anuB8193 <basani.anushka8193@gmail.com>
2025-02-10 13:56:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ const fall_2024 = 41234 + 7854 + 3001
 const fall_2023 = 40483 + 7780 + 3053
 let perc_change = (fall_2024 - fall_2023) / fall_2023
 console.log(Math.round(perc_change * 1000))
+
+const nintendo_bits=8
+const super_nintendo_bits=16
+let total_bits= nintendo_bits*super_nintendo_bits
+console.log(total_bits)
