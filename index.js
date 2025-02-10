fdeabd218c57aae41807290f1b59cf82f5f4562a
Navya Singh <98058638+singhn13@users.noreply.github.com>
2025-02-10 13:50:56 -0500

    Enrollment percent change

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const dist_earth_moon = 238855
 const moon_diameter = 2159.1
 let num_moons = dist_earth_moon / moon_diameter
 console.log(Math.floor(num_moons))
+
+const fall_2024 = 41234 + 7854 + 3001
+const fall_2023 = 40483 + 7780 + 3053
+let perc_change = (fall_2024 - fall_2023) / fall_2023
+console.log(Math.round(perc_change * 1000))
