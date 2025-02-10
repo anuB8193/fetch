9ab79b37ad49c22265b8c6b0b9ee01d34803ec7b
Navya Singh <98058638+singhn13@users.noreply.github.com>
2025-02-10 13:44:18 -0500

    Calculate how many moons b/w earth and moon

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 fetch('https://msu-webdev.github.io/api/start.json')
   .then(response => response.text())
   .then(text => console.log(text))
+
+const dist_earth_moon = 238855
+const moon_diameter = 2159.1
+let num_moons = dist_earth_moon / moon_diameter
+console.log(Math.round(num_moons))
